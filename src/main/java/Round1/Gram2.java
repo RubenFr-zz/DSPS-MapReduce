9a89d818c6d01179edc73051c1ca36c4b226f667
Ruben Fratty <rubenf@post.bgu.ac.il>
2021-07-29 17:11:43 +0300

    Output: dec w1 w2 TAB c_w1_w2 c_w1 (or c_w2) N

--- src/main/java/Round1/Gram2.java
+++ src/main/java/Round1/Gram2.java
@@ -1,7 +1,6 @@
 package Round1;
 
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.BooleanWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
@@ -9,55 +8,78 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-
 public class Gram2 implements WritableComparable<Gram2> {
 
-    private Text w1;
-    private Text w2;
+    private final Text w1;
+    private final Text w2;
     private IntWritable decade;
+    private final IntWritable type;     // 0: W1    1: W2
 
-    public Gram2(Text w1, Text w2, IntWritable dec) {
+    public Gram2() {
+        w1 = new Text("*");
+        w2 = new Text("*");
+        decade = new IntWritable(-1);
+        type = new IntWritable(-1);
+    }
+
+    public Gram2(Text w1, Text w2, IntWritable dec, int type) {
         this.w1 = w1;
         this.w2 = w2;
         this.decade = new IntWritable((dec.get() / 10) * 10);
+        this.type = new IntWritable(type);
     }
 
-    public Gram2(Text w1, Text w2) {
+    public Gram2(Text w1, Text w2, int type) {
         this.w1 = w1;
         this.w2 = w2;
-        this.decade = new IntWritable(-1);
+        this.decade = new IntWritable(0);
+        this.type = new IntWritable(type);
     }
 
-    public Gram2(int dec) {
-        this.w1 = new Text("*");
-        this.w2 = new Text("*");
-        this.decade = new IntWritable(dec);
-    }
-
-    public Gram2() {
-        this(new Text("^"), new Text("^"), new IntWritable(-1));
-    }
-
+    @Override
     public int compareTo(Gram2 other) {
         // N is always "smaller"
-        // Sort by decade and then by w1 and then by w2
+        // Sort by decade and then w1 < w2 or w2 < w1
         // if this.w1 == other.w1, put <w1, *> before <w1,w2>
         int res;
 
-        if ((res = this.decade.get() - other.decade.get()) != 0)
-            return res;
-        else if (this.is_N() && !other.is_N())
+        if (this.is_N() && !other.is_N())      // N
             return -1;
-        else if (!this.is_N() && other.is_N())
+        else if (!this.is_N() && other.is_N())      // N
             return 1;
-        else if ((res = this.w1.toString().compareTo(other.w1.toString())) != 0)
+        else if ((res = this.getDecade() - other.getDecade()) != 0)
             return res;
-        else if (this.w2.toString().equals("*") && !other.w2.toString().equals("*"))
+
+        else if (this.getType() == 0)
+            return this.compareTo1(other);
+        else
+            return this.compareTo2(other);
+    }
+
+    public int compareTo1(Gram2 other) {
+        int res;
+
+        if ((res = this.getW1().compareTo(other.getW1())) != 0)
+            return res;
+        else if (this.getW2().equals("*") && !other.getW2().equals("*"))
             return -1;
-        else if (!this.w2.toString().equals("*") && other.w2.toString().equals("*"))
+        else if (!this.getW2().equals("*") && other.getW2().equals("*"))
             return 1;
         else
-            return this.w2.toString().compareTo(other.w2.toString());
+            return this.getW2().compareTo(other.getW2());
+    }
+
+    public int compareTo2(Gram2 other) {
+        int res;
+
+        if ((res = this.getW2().compareTo(other.getW2())) != 0)
+            return res;
+        else if (this.getW1().equals("*") && !other.getW1().equals("*"))
+            return -1;
+        else if (!this.getW1().equals("*") && other.getW1().equals("*"))
+            return 1;
+        else
+            return this.getW1().compareTo(other.getW1());
     }
 
     @Override
@@ -65,6 +87,7 @@ public class Gram2 implements WritableComparable<Gram2> {
         w1.write(dataOutput);
         w2.write(dataOutput);
         decade.write(dataOutput);
+        type.write(dataOutput);
     }
 
     @Override
@@ -72,10 +95,11 @@ public class Gram2 implements WritableComparable<Gram2> {
         w1.readFields(dataInput);
         w2.readFields(dataInput);
         decade.readFields(dataInput);
+        type.readFields(dataInput);
     }
 
-    public IntWritable getDecade() {
-        return this.decade;
+    public int getDecade() {
+        return this.decade.get() / 10;
     }
 
     public String getW1() {
@@ -86,34 +110,30 @@ public class Gram2 implements WritableComparable<Gram2> {
         return this.w2.toString();
     }
 
+    public int getType() {
+        return this.type.get();
+    }
+
+    public Gram2 setType(int type) {
+        return new Gram2(this.w1, this.w2, this.decade, type);
+    }
+
+    public void setDecade(int dec) {
+        this.decade = new IntWritable(dec);
+    }
+
     public boolean is_N() {
         return this.w1.toString().equals("*") && this.w2.toString().equals("*");
     }
 
-    public Gram2 setDecade(int dec) {
-        this.decade = new IntWritable(dec);
-        return this;
-    }
-
-    public Gram2 setDecade(IntWritable dec) {
-        this.decade = dec;
-        return this;
-    }
-
-    public void setW1(Text new_w1) {
-        this.w1 = new_w1;
-    }
-
-    public void setW2(Text new_w2) {
-        this.w2 = new_w2;
-    }
-
     @Override
     public String toString() {
-        return this.decade + "-" + (this.decade.get() + 9) + " " + this.w1 + " " + this.w2;
+        return this.decade + " " + this.w1 + " " + this.w2;
     }
 
     public Text toText() {
         return new Text(this.toString());
     }
+
+
 }
--- src/main/java/Round1/Round1.java
+++ src/main/java/Round1/Round1.java
@@ -1,7 +1,6 @@
 package Round1;
 
-import java.io.IOException;
-
+import LocalApplication.Constants;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -18,69 +17,97 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 public class Round1 {
+
+    public static Logger logger;
 
     public static class MapperClass extends Mapper<LongWritable, Text, Gram2, IntWritable> {
 
         private int N;
+        private int sent;
 
         @Override
         public void setup(Context context) {
             N = 0;
+            sent = 0;
         }
 
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
             String[] data = value.toString().split("\t");
             String[] gram = data[0].split(" ");
             IntWritable dec = new IntWritable(Integer.parseInt(data[1]));
             IntWritable occ = new IntWritable(Integer.parseInt(data[2]));
 
-            if (gram.length != 2 || Constants.stopWords.contains(gram[0]) || Constants.stopWords.contains(gram[1]))
+            if (!isValid(gram) || sent > 10000)
+//            if (!isValid(gram))
                 context.getCounter(Constants.COUNTERS.NOT_COUNTED).increment(1L);
             else {
                 N += occ.get();
+                sent += 1;
 
-                // Broadcast to every reducer K = dec w1 *, V = occ (to count the 2grams starting with w2 in the entire corpus)
-                broadcast(new Gram2(new Text(gram[0]), new Text(gram[1])), occ, context);
+                // Broadcast to every reducer K = dec w1 *, V = occ (to count the 2grams starting with w1 in the entire corpus)
+                broadcast(new Gram2(new Text(gram[0]), new Text("*"), 0), occ, context);    // c(w1)
+                broadcast(new Gram2(new Text("*"), new Text(gram[1]), 1), occ, context);    // c(w2)
 
                 // Submit K = dec w1 w2, V = occ (to count the recurrence of this 2gram in the decade)
-                context.write(new Gram2(new Text(gram[0]), new Text(gram[1]), dec), occ);
+                context.write(new Gram2(new Text(gram[0]), new Text(gram[1]), dec, 0), occ);
+                context.write(new Gram2(new Text(gram[0]), new Text(gram[1]), dec, 1), occ);
             }
         }
 
         @Override
         public void cleanup(Context context) throws IOException, InterruptedException {
+            int numOfReducers = Constants.NumReduceTasks;
+
+            // Send to every decade the value of N
+            for (int i = 0; i < numOfReducers; ++i) {
+                context.write(new Gram2().setType(0), new IntWritable(N));
+                context.write(new Gram2().setType(1), new IntWritable(N));
+            }
+        }
+
+        // Broadcast to every decade the same K-V (except for the decade's value)
+        private void broadcast(Gram2 key, IntWritable val, Context context) throws IOException, InterruptedException {
             int curr_dec = Constants.smallest_year;
 
             while (curr_dec <= Constants.largest_year) {
-                context.write(new Gram2(curr_dec), new IntWritable(N));
+                key.setDecade(curr_dec);
+                context.write(key, val);
                 curr_dec += 10;
             }
         }
 
-        private void broadcast(Gram2 key, IntWritable val, Context context) throws IOException, InterruptedException {
-            key.setW2(new Text("*"));
-            int curr_dec = Constants.smallest_year;
+        private boolean isValid(String[] gram) {
+            return gram.length == 2 &&
+                    isHebrew(gram[0]) &&
+                    isHebrew(gram[1]);
+        }
 
-            while (curr_dec <= Constants.largest_year) {
-                context.write(key.setDecade(curr_dec), val);
-                curr_dec += 10;
-            }
+        private boolean isHebrew(String word) {
+            return word.matches ("^[א-ת]+$") && !Constants.stopWords.contains(word);
         }
     }
 
     public static class ReducerClass extends Reducer<Gram2, IntWritable, Text, Text> {
 
+        private IntWritable N;
         private IntWritable lastW1_occ;
+        private IntWritable lastW2_occ;
 
         @Override
         public void setup(Context context) {
+            N = new IntWritable();
             lastW1_occ = new IntWritable();
+            lastW2_occ = new IntWritable();
         }
 
         @Override
         public void reduce(Gram2 key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
             // 1. Merge Counts
             int w1_w2_occ = 0;
 
@@ -89,16 +116,21 @@ public class Round1 {
 
             // 2. Check if N
             if (key.is_N())
-                context.write(key.toText(), new Text(String.valueOf(w1_w2_occ)));
+                N.set(w1_w2_occ);
 
-                // 3. Check if new W1
+            // 3. Check if new W1
             else if (key.getW2().equals("*"))
                 lastW1_occ.set(w1_w2_occ);
 
-                // 4. If not add c(w1) to the value -> dec w1 w2 TAB c(w1,w2) c(w1)
-            else
-                context.write(key.toText(), new Text(w1_w2_occ + " " + lastW1_occ));
+            // 4. Check if new W2
+            else if (key.getW1().equals("*"))
+                lastW2_occ.set(w1_w2_occ);
 
+            // 5. If not add c(w1/2) to the value -> dec w1 w2 TAB c(w1,w2) c(w1/2) N
+            else {
+                IntWritable occ = key.getType() == 0 ? lastW1_occ : lastW2_occ;
+                context.write(key.toText(), new Text(w1_w2_occ + " " +  occ + " " + N));
+            }
         }
 
         @Override
@@ -110,10 +142,12 @@ public class Round1 {
 
         @Override
         public void reduce(Gram2 key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
             int w1_w2_occ = 0;
 
             for (IntWritable value : values)
                 w1_w2_occ += value.get();
+
             context.write(key, new IntWritable(w1_w2_occ));
         }
     }
@@ -123,21 +157,28 @@ public class Round1 {
         // Ensure that keys with the same decade are directed to the same reducer
         @Override
         public int getPartition(Gram2 key, IntWritable value, int numPartitions) {
-            int dec = key.getDecade().get() / 10;
-            return 200 - dec;
-        }
+            int res = key.getDecade() % Constants.NumReduceTasks;
 
+            if (key.getType() == 1)
+                res += Constants.NumReduceTasks;
+
+            return res;
+        }
     }
 
     public static void main(String[] args) throws Exception {
-        Logger logger = LoggerFactory.getLogger(Round1.class);
 
-        if (args.length != 2) {
+        logger = LoggerFactory.getLogger(Round1.class);
+
+        if (args.length != 3) {
             logger.error("Usage: java -jar Round1.jar <input> <output>\n");
+            for (int i = 0; i < args.length; i++)
+                logger.error("Argument " + i + ": " + args[i]);
             System.exit(-1);
         }
 
         Configuration conf = new Configuration();
+//        conf.set("minPmi", args[2]);
 
         Job job = Job.getInstance(conf, "Round 1");
         job.setJarByClass(Round1.class);
@@ -153,7 +194,7 @@ public class Round1 {
 
         // Set reducer
         job.setReducerClass(ReducerClass.class);
-        job.setNumReduceTasks(42);
+        job.setNumReduceTasks(Constants.NumReduceTasks * 2);
 
         // Set Mapper's Output format for key-value pair
         job.setMapOutputKeyClass(Gram2.class);
@@ -165,11 +206,12 @@ public class Round1 {
 
         // Input and Output format for data
         job.setInputFormatClass(TextInputFormat.class);
+//        job.setInputFormatClass(SequenceFileInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
         // File Input and Output paths
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
         // Starting...
         logger.warn("Starting Round 1...");
